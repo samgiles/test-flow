21cb36a9f5f8c7d61f37930af60b1e86a94cd4d7
samgiles <sam.giles@form3.tech>
2019-04-11 11:20:09 +0100

    Travis

---
+++ mergedown.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env
+
+set -e
+
+if [ -z ${GITHUB_TOKEN} ]; then
+  echo "Missing GITHUB_TOKEN. Aborting merge."
+  exit
+fi
+
+if [ -z ${GITHUB_REPO} ]; then
+  echo "Missing target github repo in the form 'user/name'"
+  exit
+fi
+
+export GIT_COMMITTER_EMAIL='jeeves@form3.tech'
+export GIT_COMMITTER_NAME='JeevesOnCI'
+
+# Merge targets -> current=target
+export master=test
+export test=develop
+
+merge_target=${!TRAVIS_BRANCH}
+
+if [ -z ${merge_target} ]; then
+  echo "No merge target for branch ${TRAVIS_BRANCH}. Cancelling automerge"
+  exit
+fi
+
+# Since Travis does a partial checkout, we need to get the whole thing
+repo_temp=$(mktemp -d)
+git clone "https://github.com/$GITHUB_REPO" "$repo_temp"
+
+cd "$repo_temp"
+
+echo "Checking out ${merge_target}" 
+git checkout ${merge_target}
+
+echo "Merging ${TRAVIS_COMMIT}"
+git merge --ff-only "$TRAVIS_COMMIT"
+
+echo "Pushing to ${GITHUB_REPO}"
+
+push_uri="https://$GITHUB_TOKEN@github.com/$GITHUB_REPO"
+
+# Redirect to /dev/null to avoid secret leakage
+echo "git push \"${push_uri}\" \"${merge_target}\" >/dev/null 2>&1"
